d382a5a9057467f0a77b4052e632f220c789b62a
WilhelmVon500 <167651656+WilhelmVon500@users.noreply.github.com>
2026-05-13 22:33:55 +0300

    Update data.js

--- out/html/data.js
+++ out/html/data.js
@@ -1,5 +1,13 @@
 const tooltipList = [
     {
+      searchString: "NLP",
+      explanationText: "National Liberal Party <br> <i>Nationalliberale Partei</i>"
+    },
+    {
+      searchString: "VLDP",
+      explanationText: "United Liberal Democratic Party <br> <i>Vereinigte Liberal-Demokratische Partei</i>"
+    },
+    {
       searchString: "KPD",
       explanationText: "Communist Party of Germany <br> <i>Kommunistische Partei Deutschlands</i>"
     },
@@ -12,7 +20,41 @@ const tooltipList = [
       explanationText: "Social Democratic Party <br> <i>Sozialdemokratische Partei Deutschlands</i>"
     },
     {
+      searchString: "RDP",
+      explanationText: "Radical Democratic Party <br> <i>Radikaldemokratische Partei</i>"
+    },
+    {
       searchString: "FVP",
       explanationText: "Progressive People's Party <br> <i>Fortschrittliche Volkspartei</i>"
+    }
+];
+const colourList = [
+    {
+        word: "NLP",
+        style: "color: var(--nlp-color); font-weight: bold;",
+    },
+    {
+        word: "VLDP",
+        style: "color: var(--nlp-color); font-weight: bold;",
+    }
+    {
+        word: "KPD",
+        style: "color: #8B0000; font-weight: bold;",
+    },
+    {
+        word: "USPD",
+        style: "color: #D47B9B; font-weight: bold;",
+    },
+    {
+        word: "SPD",
+        style: "color: #E3000F; font-weight: bold;",
+    },
+    {
+        word: "RDP",
+        style: "color: var(--fvp-color); font-weight: bold;",
+    },
+    {
+        word: "FVP",
+        style: "color: var(--fvp-color); font-weight: bold;",
     }
 ];
