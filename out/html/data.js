008aa45f3750a68a8d6a335a75e08d0b1413300d
WilhelmVon500 <167651656+WilhelmVon500@users.noreply.github.com>
2026-05-13 22:43:57 +0300

    Update data.js

--- out/html/data.js
+++ out/html/data.js
@@ -26,7 +26,51 @@ const tooltipList = [
     {
       searchString: "FVP",
       explanationText: "Progressive People's Party <br> <i>Fortschrittliche Volkspartei</i>"
-    }
+    },
+    {
+      searchString: "BB",
+      explanationText: "Bavarian Peasants' League <br> <i>Bayerischer Bauernbund</i>"
+    },
+    {
+      searchString: "Z",
+      explanationText: "Centre Party <br> <i>Zentrum</i>"
+    },
+    {
+      searchString: "BVP",
+      explanationText: "Bavarian People's Party <br> <i>Bayerische Volkspartei</i>"
+    },
+    {
+      searchString: "CSVD",
+      explanationText: "Christian Social People's Service <br> <i>Christlich-Sozialer Volksdienst</i>"
+    },
+    {
+      searchString: "WP",
+      explanationText: "Economic Party of the German Middle Classes <br> <i>Wirtschaftspartei des deutschen Mittelstandes</i>"
+    },
+    {
+      searchString: "RLB",
+      explanationText: "National Rural League <br> <i>Reichslandbund</i>"
+    },
+    {
+      searchString: "KVP",
+      explanationText: "Conservative People's Party <br> <i>Konservative Volkspartei</i>"
+    },
+    {
+      searchString: "DNVP",
+      explanationText: "German National People's Party <br> <i>Deutschnationale Volkspartei</i>"
+    },
+    {
+      searchString: "KSWR",
+      explanationText: "Black-White-Red Battle Front <br> <i>Kampffront Schwarz-Weiss-Rot</i>"
+    },
+    {
+      searchString: "DAP",
+      explanationText: "German Workers' Party <br> <i>Deutsche Arbeiterpartei</i>"
+    },
+    {
+      searchString: "Others",
+      explanationText: "Other Parties <br> <i>Sonstige Parteien</i>"
+    },
 ];
 const colourList = [
     {
@@ -36,7 +80,7 @@ const colourList = [
     {
         word: "VLDP",
         style: "color: var(--nlp-color); font-weight: bold;",
-    }
+    },
     {
         word: "KPD",
         style: "color: #8B0000; font-weight: bold;",
@@ -56,5 +100,49 @@ const colourList = [
     {
         word: "FVP",
         style: "color: var(--fvp-color); font-weight: bold;",
+    },
+    {
+        word: "BB",
+        style: "color: #38C538; font-weight: bold;",
+    },
+    {
+        word: "Z",
+        style: "color: #000000; font-weight: bold;",
+    },
+    {
+        word: "BVP",
+        style: "color: #69A2BE; font-weight: bold;",
+    },
+    {
+        word: "CSVD",
+        style: "color: #1DB4E1; font-weight: bold;",
+    },
+    {
+        word: "WP",
+        style: "color: #B8996F; font-weight: bold;",
+    },
+    {
+        word: "RLB",
+        style: "color: #52AE6F; font-weight: bold;",
+    },
+    {
+        word: "KVP",
+        style: "color: #455B95; font-weight: bold;",
+    },
+    {
+        word: "DNVP",
+        style: "color: #3F7BC1; font-weight: bold;",
+    },
+    {
+        word: "KSWR",
+        style: "color: #003755; font-weight: bold;",
+    },
+    {
+        word: "DAP",
+        style: "color: #096300; font-weight: bold;",
+    },
+    {
+        word: "Others",
+        style: "color: #909090; font-weight: bold;",
     }
 ];
