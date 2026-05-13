0107b09de4eb0fbfec8384d9425ecb89f5fcd8d8
WilhelmVon500 <167651656+WilhelmVon500@users.noreply.github.com>
2026-05-13 21:41:42 +0300

    Create data.js

---
+++ out/html/data.js
@@ -0,0 +1,12 @@
+const tooltipList = [
+    {
+      searchString: "SPD",
+      explanationText: "<img src=img/spd.png> Social Democratic Party <br> (Sozialdemokratische Partei)"
+    }
+];
+const colourList = [
+    {
+        word: "SPD",
+        style: "color: #E3000F; font-weight: bold;",
+    }
+];
